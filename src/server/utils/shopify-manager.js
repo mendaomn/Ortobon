0481d9bc89dc739c09760d931e2feff72b4524cc
Alessandro Menduni <menduni6@gmail.com>
2018-12-10 18:23:45 +0100

    Add inventory_management to variant's properties

--- src/server/utils/shopify-manager.js
+++ src/server/utils/shopify-manager.js
@@ -1,48 +1,54 @@
-const fetch = require('node-fetch');
-const Client = require('shopify-buy');
+const fetch = require('node-fetch')
+const Client = require('shopify-buy')
 const SECRETS = require('../config/secrets')
 
-global.fetch = fetch;
+global.fetch = fetch
 
-const DOMAIN = 'ortobon.myshopify.com';
-const URL_PRE = SECRETS.SHOPIFY_HOST;
-const CREATE_PRODUCT_URL = `${URL_PRE}/admin/products.json`;
-const GET_PRODUCT_URL = id => `${URL_PRE}/admin/products/${id}`;
-const COLLECTS_URL = `${URL_PRE}/admin/collects.json`;
+const DOMAIN = 'ortobon.myshopify.com'
+const URL_PRE = SECRETS.SHOPIFY_HOST
+const CREATE_PRODUCT_URL = `${URL_PRE}/admin/products.json`
+const GET_PRODUCT_URL = id => `${URL_PRE}/admin/products/${id}`
+const COLLECTS_URL = `${URL_PRE}/admin/collects.json`
 
-const BUY_BUTTON_COLLECTION_ID = 36216700986;
+const BUY_BUTTON_COLLECTION_ID = 36216700986
 
 class ShopifyManager {
-
-  static get SDK_URL() {
-    return process.env.NODE_ENV === 'production' ? 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js' : 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.js'
+  static get SDK_URL () {
+    return process.env.NODE_ENV === 'production'
+      ? 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js'
+      : 'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.js'
   }
 
   static async init () {
-    const client = Client.buildClient( {
+    const client = Client.buildClient({
       domain: DOMAIN,
       storefrontAccessToken: '859e203775d5b93d20291900d4758f7c'
-    } );
-    
+    })
+
     // ShopifyManager.products = await client.product.fetchAll();
   }
 
-  static async update( data ) {
-    
-    return new Promise( async (resolve, reject) => {
-      const shouldCreateProduct = await ShopifyManager.shouldCreateProduct( data );
+  static async update (data) {
+    return new Promise(async (resolve, reject) => {
+      const shouldCreateProduct = await ShopifyManager.shouldCreateProduct(
+        data
+      )
 
       const payload = {
-        product :{
+        product: {
           title: data.name,
-          variants: [{
-            option1: data.pricelabel || '0.5 Kg',
-            price: data.price,
-            // weight: data.weight || 0.5
-          }],
-          images: [{
-            src: data.image.secure_url
-          }]
+          variants: [
+            {
+              option1: data.pricelabel || '0.5 Kg',
+              price: data.price,
+              inventory_management: null
+            }
+          ],
+          images: [
+            {
+              src: data.image.secure_url
+            }
+          ]
         }
       }
 
@@ -50,108 +56,104 @@ class ShopifyManager {
         payload.product.variants.push({
           option1: data.offerlabel,
           price: data.offer,
-          // weight: data.weight || 0.5
+          inventory_management: null
         })
       }
-      
 
       if (shouldCreateProduct) {
-        const id = await ShopifyManager.createProduct( payload )
+        const id = await ShopifyManager.createProduct(payload)
         await ShopifyManager.associateToCollection(id)
-        resolve(id);
+        resolve(id)
       } else {
-        const putRes = await ShopifyManager.put( GET_PRODUCT_URL( data.shopifyID ), payload )
-        resolve( data.shopifyID )
+        const putRes = await ShopifyManager.put(
+          GET_PRODUCT_URL(data.shopifyID),
+          payload
+        )
+        resolve(data.shopifyID)
       }
-
     })
   }
 
-  static async shouldCreateProduct( data ) {
+  static async shouldCreateProduct (data) {
     const { shopifyID } = data
-    
-    if (!shopifyID) return true;
 
-    const getRes = await ShopifyManager.get( GET_PRODUCT_URL( shopifyID ) )
-      
-    if (getRes.status === 404) return true;
+    if (!shopifyID) return true
 
-    return false;
+    const getRes = await ShopifyManager.get(GET_PRODUCT_URL(shopifyID))
+
+    if (getRes.status === 404) return true
+
+    return false
   }
 
-  static async createProduct( data ) {
-    const res = await ShopifyManager.post( CREATE_PRODUCT_URL, data );
-    const { product } = await res.json().catch( errorHandler );
+  static async createProduct (data) {
+    const res = await ShopifyManager.post(CREATE_PRODUCT_URL, data)
+    const { product } = await res.json().catch(errorHandler)
     const { id } = product
 
     return id
   }
 
-  static async associateToCollection( id ) {
+  static async associateToCollection (id) {
     const payload = {
       collect: {
         product_id: id,
         collection_id: BUY_BUTTON_COLLECTION_ID
       }
     }
-    
-    return ShopifyManager.post( COLLECTS_URL, payload)
+
+    return ShopifyManager.post(COLLECTS_URL, payload)
   }
 
-  static async remove( data ) {
-    return ShopifyManager.delete( GET_PRODUCT_URL(data.shopifyID) );
+  static async remove (data) {
+    return ShopifyManager.delete(GET_PRODUCT_URL(data.shopifyID))
   }
 
-  static async get( url ) {
-    return fetch( url, {
+  static async get (url) {
+    return fetch(url, {
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json'
       },
       method: 'GET'
-    } )
-    .catch( errorHandler )
+    }).catch(errorHandler)
   }
 
-  static async post( url, data ) {
-    return fetch( url, {
+  static async post (url, data) {
+    return fetch(url, {
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json'
       },
       method: 'POST',
-      body: JSON.stringify( data )
-    } )
-    .catch( errorHandler )
+      body: JSON.stringify(data)
+    }).catch(errorHandler)
   }
 
-  static async put( url, data ) {
-    return fetch( url, {
+  static async put (url, data) {
+    return fetch(url, {
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json'
       },
       method: 'PUT',
-      body: JSON.stringify( data )
-    } )
-    .catch( errorHandler )
+      body: JSON.stringify(data)
+    }).catch(errorHandler)
   }
 
-  static async delete( url ) {
-    return fetch( url, {
+  static async delete (url) {
+    return fetch(url, {
       headers: {
-        'Accept': 'application/json',
+        Accept: 'application/json',
         'Content-Type': 'application/json'
       },
       method: 'DELETE'
-    } )
-    .catch( errorHandler )
+    }).catch(errorHandler)
   }
 }
 
-function errorHandler( err ) {
-  console.error( err );
+function errorHandler (err) {
+  console.error(err)
 }
 
-
-module.exports = ShopifyManager;
+module.exports = ShopifyManager
